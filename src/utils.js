4bb08df51161295de8dcf617b987a2cac46ced07
Nabil Enayet <nabil.enayet@nuvolo.com>
2020-04-22 10:25:02 -0400

    Add random username generator

--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,28 @@
 export function randomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+export function genRandomUserName() {
+  const adj = [
+    'Guilty',
+    'Rotten',
+    'Supercool',
+    'Bearded',
+    'Filthy',
+    'Stubborn',
+  ];
+  const nouns = [
+    'Spark',
+    'Apple',
+    'Potato',
+    'Dragon',
+    'Kitty',
+    'Brethren',
+    'Fungus',
+  ];
+
+  const randAdj = adj[Math.floor(Math.random() * adj.length)];
+  const randNoun = nouns[Math.floor(Math.random() * nouns.length)];
+
+  return randAdj + randNoun;
+}
