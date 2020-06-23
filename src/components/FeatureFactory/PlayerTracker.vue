03c8be258e498550ace69b786a124b97c9813f80
Nabil Enayet <nenayet@NENAYET-MACBOOK16.local>
2020-06-23 15:18:27 -0400

    Changes to PlayerTracker component to work with network state

--- src/components/FeatureFactory/PlayerTracker.vue
+++ src/components/FeatureFactory/PlayerTracker.vue
@@ -5,25 +5,44 @@
       <div>
         <b-tag>Actions: {{ actionDeck.deck.length }}</b-tag>
         <b-tag>Drawn: {{ actionDeck.drawn.length }}</b-tag>
+        <b-tag type="is-primary" v-if="drafting">Drafting</b-tag>
         <b-button v-on:click="shuffle()">Reshuffle</b-button>
-        <b-button v-on:click="addPlayer()">Add Player</b-button>
+        <b-button v-on:click="setDrafting()">Toggle Card Drafting</b-button>
+        <!-- <b-button v-on:click="addPlayer()">Add Player</b-button> -->
       </div>
       <div>
         <b-tabs v-model="activePlayer">
           <template v-for="(player, i) in players">
-            <b-tab-item :label="player.name" :key="i"
-              ><div class="columns">
-                <div class="column" v-for="(card, c) in player.hand" :key="c">
-                  <b-button
-                    class="card"
-                    :style="{ border: `solid 4px ${card.background}` }"
-                    v-on:click="playCard(i, c)"
-                  >
-                    {{ card.name }}
-                  </b-button>
+            <b-tab-item :label="getPlayerLabel(player)" :key="i">
+              <div v-if="userName === player.name">
+                <h4>Hand</h4>
+                <div class="columns">
+                  <div class="column" v-for="(card, c) in player.hand" :key="c">
+                    <b-button
+                      class="card"
+                      :style="{ border: `solid 4px ${card.background}` }"
+                      v-on:click="playCard(i, c)"
+                      >{{ card.name }}</b-button
+                    >
+                  </div>
                 </div>
-              </div></b-tab-item
-            >
+              </div>
+              <h4>Drawn</h4>
+              <div
+                v-for="(card, k) in player.reserve"
+                :key="`${card.name}_${k}`"
+              >
+                <div
+                  :style="{
+                    border: `solid 4px ${card.background}`,
+                    textAlign: 'left',
+                  }"
+                >
+                  <b-tag>Day {{ k + 1 }}</b-tag
+                  >{{ card.name }}
+                </div>
+              </div>
+            </b-tab-item>
           </template>
         </b-tabs>
       </div>
@@ -31,6 +50,7 @@
   </div>
 </template>
 <script>
+import { mapState } from 'vuex';
 import GameStore from './gameStore';
 
 export default {
@@ -56,13 +76,29 @@ export default {
     shuffle() {
       GameStore.commit('shuffleActions');
     },
+    setDrafting() {
+      GameStore.commit('toggleDrafting');
+    },
+    getPlayerLabel(player) {
+      const user = this.userName;
+      if (player.name === user) {
+        return `😎    ${player.name}    😎`;
+      }
+      return player.name;
+    },
   },
   data() {
     return {
-      players: GameStore.state.players,
-      actionDeck: GameStore.state.actions,
       activePlayer: 0,
     };
+  },
+  computed: {
+    drafting: () => GameStore.state.drafting,
+    players: () => GameStore.state.players,
+    actionDeck: () => GameStore.state.actions,
+    ...mapState({
+      userName: (s) => s.userInfo.userName,
+    }),
   },
 };
 </script>
